944cc6d2bf16ac09baee0c6c7498c6ee49860216
Talha-Bayansar <talha.bayansar@hotmail.com>
2020-09-07 04:17:11 +0200

    created new component Header.js

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <nav className="header">
+
+            {/* logo on the left -> img */}
+            <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="" />
+            {/* search box */}
+            {/* 3 links */}
+            {/* basket icon with number */}
+        </nav>
+    )
+}
+
+export default Header
