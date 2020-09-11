d2abbec2b40cd0e8c9f3759a07373e7bf5fa7a65
Talha-Bayansar <talha.bayansar@hotmail.com>
2020-09-11 16:45:26 +0200

    adding items to basket implemented and subtotal will update

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,16 +1,31 @@
 import React from "react";
 import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
+import { useStateValue } from "./StateProvider";
 
 function Subtotal() {
+  const [{ basket }, dispatch] = useStateValue();
+
+  /* let totalPrice = 0;
+
+  for (let i = 0; i < basket.length; i++) {
+    totalPrice += basket[i].price;
+  } */
+
   return (
     <div className="subtotal">
       <CurrencyFormat
         renderText={(value) => (
           <>
             <p>
-              Subtotal (0 items):
-              <strong> 0</strong>
+              Subtotal ({basket?.length} items):
+              <strong>
+                {" "}
+                $
+                {basket.reduce((total, item) => {
+                  return total + item.price;
+                }, 0)}
+              </strong>
             </p>
             <small className="subtotal__gift">
               <input type="checkbox" />
